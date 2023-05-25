5f663d42fbefa9937a65ec927ffd60a72992e107
NobodyToListen <giacalone293@gmail.com>
2023-05-25 12:08:34 +0200

    Aggiunto supporto ai gopher selector.

--- protocol_controller/test/gopher_controller_test.dart
+++ protocol_controller/test/gopher_controller_test.dart
@@ -7,17 +7,17 @@ import 'dart:io';
 
 void main() {
   test('Aprire il socket', () async {
-    final gopher_controller = GopherController("sdf.org", 70, "/");
+    final gopher_controller = GopherController("sdf.org", 70, "/", "0");
     Socket socket = await gopher_controller.get_socket();
     await socket.close();
   });
 
   test('Letture dal socket', () async {
-    final gopher_controller = GopherController("sdf.org", 70, "/");
+    final gopher_controller = GopherController("sdf.org", 70, "/", "");
     String data = await gopher_controller.make_request();
 
     final Logger my_logger = Logger('mylogger');
-    final log_file = File('log.txt');
+    final log_file = File('filelog.txt');
 
     my_logger.onRecord.listen((record) {
       log_file.writeAsStringSync(record.message);
@@ -30,7 +30,7 @@ void main() {
 
   test('Motore di ricerca', () async {
     final gopher_controller =
-        GopherController("gopher.floodgap.com", 70, "/v2/vs");
+        GopherController("gopher.floodgap.com", 70, "/v2/vs", "");
     String data = await gopher_controller.make_request("programming");
 
     final Logger my_logger = Logger('mylogger');
@@ -44,4 +44,38 @@ void main() {
 
     expect(data.contains("floodgap"), true);
   });
+
+  test('Selector', () async {
+    final gopher_controller =
+        GopherController("gopher.floodgap.com", 70, "/maps/", "1");
+    String data = await gopher_controller.make_request();
+
+    
+    final Logger my_logger = Logger('mylogger');
+    final log_file = File('maplog.txt');
+
+    my_logger.onRecord.listen((record) {
+      log_file.writeAsStringSync(record.message);
+    });
+
+    my_logger.info(data);
+
+    
+    expect(data.contains("maps"), true);
+  });
+
+/*  test('File di testo', () async {
+    final gopher_controller =
+        GopherController("home.quix.us", 70, "/text/programming/author.faq");
+    String data = await gopher_controller.make_request();
+
+    final Logger my_logger = Logger('mylogger');
+    final log_file = File('filelog.txt');
+
+    my_logger.onRecord.listen((record) {
+      log_file.writeAsStringSync(record.message);
+    });
+
+    my_logger.info(data);
+  }, timeout: Timeout(Duration(minutes: 1)));*/
 }
